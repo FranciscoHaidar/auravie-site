76920917ea7e3d9a1d161cc2f4f820d05994c11f
FranciscoHaidar <franciscohaidar@outlook.com>
2026-03-22 11:10:00 -0300

    fix: hardcode anon keys for vercel

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Hardcoding public keys for Vercel deployment without the need for Dashboard configuration
+const supabaseUrl = "https://ebudnfpecnjqtpbxstqx.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVidWRuZnBlY25qcXRwYnhzdHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0Njc1MjQsImV4cCI6MjA4OTA0MzUyNH0.Rtv3rvULx5cDpiAHw1Iq7lf-GIXyJ4Hvyls97AgxK0E";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
